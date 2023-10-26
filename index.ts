5ce32b3abff1ce82348c228cff51c1d4145d3df5
pulsarfox <pascal@plumia.net>
2023-10-26 10:03:49 +0200

    Some routing tests

--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log("Hello via Bun!");
+type Techno = {
+  id: number,
+  name: string,
+  type: 'Front' | 'Back' | 'Full-stack'
+}
+
+const technos: Techno[] = [
+  {
+    id: 1,
+    name: 'Node',
+    type: 'Back'
+  },
+  {
+    id: 2,
+    name: 'Bun',
+    type: 'Back'
+  },
+  {
+    id: 3,
+    name: 'Qwik',
+    type: 'Full-stack'
+  }
+]
+
+Bun.serve({
+  hostname: "localhost",
+  port: 3000,
+  fetch(req: Request): Response | Promise<Response> {
+
+    const url = new URL(req.url)
+
+    if (url.pathname === '/ping') {
+      return new Response('pong')
+    }
+
+    if (url.pathname === '/technos') {
+      return new Response(JSON.stringify(technos));
+    }
+
+    return new Response('Not found')
+  }
+})
